2f0826733557767c451b01a50797b63c0c0b4a38
Cyrus Yiu <UnsignedArduino@outlook.com>
2023-10-16 22:16:08 -0400

    Icons, fix regenerate API token button behavior

--- TinyTestWebUI/src/components/Settings/RoleBadges.tsx
+++ TinyTestWebUI/src/components/Settings/RoleBadges.tsx
@@ -10,12 +10,21 @@ export default function RoleBadges() {
     return (
       <>
         {userContext.verified ? (
-          <span className="badge text-bg-secondary me-1">Verified</span>
+          <span className="badge text-bg-secondary me-1">
+            <i className="bi-check me-1" />
+            Verified
+          </span>
         ) : (
-          <span className="badge text-bg-warning me-1">Unverified</span>
+          <span className="badge text-bg-warning me-1">
+            <i className="bi-x me-1" />
+            Unverified
+          </span>
         )}
         {userContext.admin ? (
-          <span className="badge text-bg-primary me-1">Administrator</span>
+          <span className="badge text-bg-primary me-1">
+            <i className="bi-shield-shaded me-1" />
+            Administrator
+          </span>
         ) : undefined}
       </>
     );
